561849f3bf38ab1881ea773c47078a5a04d29697
夏欢 <8488351+xia-huan@user.noreply.gitee.com>
2022-09-21 17:04:03 +0800

    account-server启动类的添加，接口文档中响应码的修改

---
+++ video-api/account-server/src/main/java/cn/jasonone/controller/AccountController.java
@@ -0,0 +1,17 @@
+package cn.jasonone.controller;
+
+import cn.dev33.satoken.annotation.SaCheckPermission;
+import cn.jasonone.bean.Account;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Validated
+@RestController
+@RequestMapping("/user")
+public class AccountController {
+
+
+}
