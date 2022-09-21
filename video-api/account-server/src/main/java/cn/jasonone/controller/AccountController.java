4811f8cb2a5799557c59c26c80a826ce6eb731cb
刘柏宏 <3093305018@qq.com>
2022-09-21 18:41:48 +0800

    用户模块 修改用户昵称

--- video-api/account-server/src/main/java/cn/jasonone/controller/AccountController.java
+++ video-api/account-server/src/main/java/cn/jasonone/controller/AccountController.java
@@ -1,10 +1,14 @@
 package cn.jasonone.controller;
-import cn.dev33.satoken.annotation.SaCheckPermission;
+
 import cn.jasonone.bean.Account;
 import cn.jasonone.model.HttpResult;
 import cn.jasonone.service.AccountService;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
@@ -34,5 +38,21 @@ public class AccountController {
         }else {
             return HttpResult.error(10081,"邮箱修改失败");
         }
+    }
+
+    @PostMapping("/updateNickname")
+    public HttpResult updateNickname(@RequestBody Account account){
+        System.out.println(account);
+        Map<String, Object> rs=new HashMap<>();
+        System.out.println("55");
+        int i = accountService.updateNickname(account);
+        if(i>0){
+            rs.put("code",0);
+            rs.put("msg","昵称修改成功！");
+        }else {
+            rs.put("code",1);
+            rs.put("msg","昵称修改失败！");
+        }
+        return HttpResult.ok(rs);
     }
 }
