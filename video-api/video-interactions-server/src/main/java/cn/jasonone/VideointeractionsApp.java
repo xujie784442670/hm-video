4c415e5c0c161f380f0db3e468e4a27bc0782e43
夏欢 <8488351+xia-huan@user.noreply.gitee.com>
2022-09-23 11:09:50 +0800

    视频交互模块接口信息的编写

---
+++ video-api/video-interactions-server/src/main/java/cn/jasonone/VideointeractionsApp.java
@@ -0,0 +1,11 @@
+package cn.jasonone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideointeractionsApp {
+    public static void main(String[] args) {
+        SpringApplication.run(VideointeractionsApp.class,args);
+    }
+}
